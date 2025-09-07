fe291771ca792f66bf04ead978e6e0928b71a281
ayushinarnolia <157296162+ayushinarnolia@users.noreply.github.com>
2025-09-07 10:28:23 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,22 +1,24 @@
+// *********************************  Converisons  **************************************
+
 let score="30"
-
-console.log(typeof score);
-
+//console.log(typeof score);
 let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
+//NOTES
 //"30" => 30
 //"30abc" => NaN
 //true => 1 and false => 0
 //null => 0
 //undefined => NaN
 
+
 let isLoggedIn=1;
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
+//NOTES
 //1 => true and 0 => false
 //"" => false
 //"ayushi" => true
@@ -24,7 +26,39 @@ console.log(booleanIsLoggedIn);
 
 let num=30
 let StringNumber= String(num)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
 
 
+// *********************************  Operations  **************************************
+
+let value=3
+let negValue= -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**4);
+//console.log(10/2);
+//console.log(11%3);
+
+let str1="hello"
+let str2="ayushi"
+let str3=str1+str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1" +2+2);
+//console.log(1+2+"2");
+
+//console.log(+true); //1
+//console.log(+""); //0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
