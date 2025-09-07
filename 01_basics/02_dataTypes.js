95b79363720544b3d4f07fa3b728ebb9d707dcbb
ayushinarnolia <157296162+ayushinarnolia@users.noreply.github.com>
2025-09-07 09:53:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //trate all JS code as newer version
+
+//alert(3+3) //this doesnt work cause we are using nodejs. this will work in the browser in inspect mode tho
+
+console.log(3+3);
+console.log("Ayushiiii");
+
+let name="Ayushi"
+let age=20
+let isLoggedIn=false
+
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value => data type is object
+//undefined => value is only not defined
+//symbol => uniqueness in react
+
+
+//object
+
+console.log(typeof null);
