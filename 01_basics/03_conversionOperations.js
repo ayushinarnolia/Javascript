1bbab102a225ca55a97fd73f7a5150dd78d67528
ayushinarnolia <157296162+ayushinarnolia@users.noreply.github.com>
2025-09-07 10:04:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score="30"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"30" => 30
+//"30abc" => NaN
+//true => 1 and false => 0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true and 0 => false
+//"" => false
+//"ayushi" => true
+
+
+let num=30
+let StringNumber= String(num)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
