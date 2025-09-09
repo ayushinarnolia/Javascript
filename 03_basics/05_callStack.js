c14a519866b5fae795582f8100a43ce9359f3c7f
ayushinarnolia <157296162+ayushinarnolia@users.noreply.github.com>
2025-09-09 11:39:56 +0000

    java execution method

---
+++ 03_basics/05_callStack.js
@@ -0,0 +1,15 @@
+/*Javascript Execution Context
+
+1) Global EC->locate using this (always happen)
+single threaded
+Function EC
+Eval EC
+2) Memory Creation Phase
+3) Execution Phase
+in each funtion this sandbox is created - (new variable environment + execution thread)-once its work is done, it gets deleted
+*/
+
+/*
+CALL STACK:
+LIFO
+*/
